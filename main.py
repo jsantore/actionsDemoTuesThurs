1661dbb18714b7f9ed4f50c995aada4e62c31266
jsantore <jsantore@bridgew.edu>
2022-01-25 09:59:55 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def add_it_up(first: int, second: int):
+    return first + second
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(add_it_up(5, 6))
